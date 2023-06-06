9023d0ab8d0067add20b73660570ed253203c256
Evistix <mephistakaiwan@gmail.com>
2023-06-06 21:06:59 +0200

    initial + BOM

---
+++ src/main/java/com/evi/teamfinderauth/TeamFinderAuthApplication.java
@@ -0,0 +1,13 @@
+package com.evi.teamfinderauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFinderAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamFinderAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evi/teamfinderauth/TeamFinderAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evi.teamfinderauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFinderAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
